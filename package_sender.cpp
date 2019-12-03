ffa2dece35a0971f8d90ecacce8979a68d734878
Martin Christensen <martinchristensen1995@gmail.com>
2019-12-03 11:13:14 +0100

    Add files via upload

--- package_sender.cpp
+++ package_sender.cpp
@@ -9,10 +9,12 @@ void PackageSender::SendMessage(std::vector<std::bitset<8>> message) {
 	std::chrono::seconds waitTime(10); //Tid at vendte på ACK i sekunder
 	std::chrono::seconds wtNextPck(1); //Tid at vendte med at sende næste pakke  i sekunder
 
-	SoundGenerator soundGenerator;
 
 	for (size_t i = 0; i < toSend.size(); i++) //Send hver pakke
 	{
+		SoundGenerator soundGenerator;
+		DeFrame deFrame;
+
 		//Er der modtaget et ACK fra forrige pakke
 		//Vent i wtNextPck tid med at sende næste pakke, for at give modtager en chance for at følje med.
 		if (DeFrame::ack)
@@ -32,5 +34,7 @@ void PackageSender::SendMessage(std::vector<std::bitset<8>> message) {
 			DTMFRecorder::pauseRecording = false; // start med at lytte igen
 			std::this_thread::sleep_for(waitTime); // vent på ACK
 		}
+		//TO DO: RESET ACK
+		deFrame.ack = false;
 	}
 }
